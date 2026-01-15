1c7e9bdfa95109ab84c179747227bee440e6c15e
Pardamean <gabeputra@gmail.com>
2026-01-15 11:22:39 +0700

    Fix 419 by excluding api routes from CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,7 +6,10 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
+    /**
+     * URIs that should be excluded from CSRF verification.
+     */
     protected $except = [
-        'absensi/ajax',
+        'api/*',
     ];
 }
