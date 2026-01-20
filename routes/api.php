11a3522c9911be67b0ad141be5e03a15843374d3
Pardamean <gabeputra@gmail.com>
2026-01-20 23:19:33 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -11,33 +11,6 @@ use App\Models\PengaturanKantor;
 |--------------------------------------------------------------------------
 */
 
-Route::post('/login', [AuthController::class, 'apiLogin'])->name('api.login');
-
-Route::post('/absensi', [AbsensiController::class, 'simpanAjax'])
-    ->name('api.absensi');
-
-Route::get('/lokasi-kantor', function () {
-    $kantor = PengaturanKantor::first();
-
-    return response()->json([
-        'latitude'  => (float) $kantor->latitude,
-        'longitude' => (float) $kantor->longitude,
-        'radius'    => (int) $kantor->radius,
-    ]);
-});
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AbsensiController;
-use App\Models\PengaturanKantor;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
-
 // LOGIN API
 Route::post('/login', [AuthController::class, 'apiLogin'])
     ->name('api.login');
